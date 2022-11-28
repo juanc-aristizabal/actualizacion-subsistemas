0c544250d5f0a35ba588844eab7927241f5dc229
Juan carlos <juan.aristizabal@and.gov.co>
2022-11-27 19:11:24 -0500

    update

--- install.sh
+++ install.sh
@@ -3,15 +3,18 @@
 
 sudo apt install ruby
 
+mkdir /var/lib/xroad/public/key
+chgrp xroad /var/lib/xroad/public/key
+chown xroad /var/lib/xroad/public/key
+cp ./files/AND_cerrificate.crt /var/lib/xroad/public/key/AND_cerrificate.crt
+cp ./files/AND_private.key /var/lib/xroad/public/key/AND_private.key
+
 cp ./files/update.rb  /var/lib/xroad/public/update.rb
 chgrp xroad /var/lib/xroad/public/update.rb
 chown xroad /var/lib/xroad/public/update.rb
 chmod 755 /var/lib/xroad/public/update.rb
 
 cat ./files/xroad-center > /etc/cron.d/xroad-center
-#cp ./files/xroad-center /etc/cron.d/center-service
-#chgrp xroad /etc/cron.d/xroad-center
-#chown xroad /etc/cron.d/xroad-center
 
 echo "Actualización de subsistemas activado"
 echo "every thing it's ok, life is good..."
