bc8ef339d8d476854ef48b98cbec3635e4307a6d
Juan carlos <juan.aristizabal@and.gov.co>
2022-11-27 19:33:58 -0500

    update

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ chmod 755 /var/lib/xroad/public/update.rb
 
 cat ./files/xroad-center > /etc/cron.d/xroad-center
 
-echo "Actualización de subsistemas activado"
+echo "subsystem update enabled"
 echo ""
 echo "every thing it's ok, life is good..."
 echo ""
